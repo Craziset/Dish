2815a3f8dd1e3dc3cd3f7642df1ada6709256cef
crazyLemon <Corepass@corepassdemacbook-air.local>
2016-06-22 11:12:25 +0800

    Initial Commit

---
+++ Dish/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dish
+//
+//  Created by Corepass on 16/6/22.
+//  Copyright © 2016年 LONG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
